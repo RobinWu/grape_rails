15d085fe856fc59c6cd3297c519a5029226aa276
robin <robin.wu.cn@gmail.com>
2018-03-02 09:13:08 +0800

    add grape-jbuilder

--- app/api/twitter/apiv1.rb
+++ app/api/twitter/apiv1.rb
@@ -2,12 +2,19 @@ module Twitter
   class APIV1 < Grape::API
     version 'v1'
     format :json
+    formatter :json, Grape::Formatter::Jbuilder
     prefix :api
 
     resource :statuses do
       desc 'Return a public timeline.'
       get :public_timeline do
         [1,2]
+      end
+
+      desc '/statuses/:id'
+      get ':id', jbuilder: 'statuses/show.jbuilder' do
+        @user = Status.find(params[:id])
+        @user.created_at = Time.now
       end
     end
 
